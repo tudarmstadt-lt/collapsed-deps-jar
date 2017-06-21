10f73b14349f757e46bf0288c27abcf1e234de73
Alexander Panchenko <panchenko.alexander@gmail.com>
2017-06-21 17:59:08 +0200

    install script

---
+++ intall-jar-locally.sh
@@ -0,0 +1,7 @@
+mvn install:install-file \
+   -Dfile=org.jobimtext.collapsing.jar \
+   -DgroupId=jobimtext \
+   -DartifactId=org.jobimtext.collapsing \
+   -Dversion=0.0.1 \
+   -Dpackaging=jar \
+   -DgeneratePom=true
